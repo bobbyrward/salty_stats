31d7dc14ec0a2c7643d922e3ef513c601d1233b3
Bobby R. Ward <bobbyrward@gmail.com>
2013-08-25 23:43:23 -0400

    Re-enable focus on prediction view

--- salty_stats/prediction_view.py
+++ salty_stats/prediction_view.py
@@ -1,6 +1,5 @@
 import logging
 
-from PySide import QtCore
 from PySide import QtGui
 
 from salty_stats.predictor import predict_winner
@@ -12,7 +11,6 @@ class PredictionView(QtGui.QTextEdit):
     def __init__(self, parent):
         super(PredictionView, self).__init__(parent)
         self.setReadOnly(True)
-        self.setFocusPolicy(QtCore.Qt.NoFocus)
 
         self.player1 = None
         self.player2 = None
